fced330304c93747099311dfe93c61240f728eb2
Tonny <Azazith@hotmail.com>
2020-08-05 22:40:55 +0200

    Added gitignore + mainMenu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("startMenu"));
+        scene = new Scene(loadFXML("mainMenu"));
         stage.setScene(scene);
         stage.setTitle("Entask");
         stage.setMinWidth(480);
